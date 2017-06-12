4bb7daca2aadf0af61a4cbe6888dbf722e217649
carons-cr <2738794789@qq.com>
2017-06-12 19:20:40 +0800

    学生类继承人类

--- main/person.js
+++ main/person.js
@@ -1,2 +1,9 @@
 // Write your code here
-module.exports = Person
+module.exports = Person(name,age){
+            this.name=name;
+	    this.age=age;
+	    Person.prototype.introduce=function(){
+	         var s1="My name is "+this.name+". I am "+this.age+" years old. ";
+	         return s1;
+	    }
+}
--- main/student.js
+++ main/student.js
@@ -1,3 +1,11 @@
 let Person = require('./person')
 // Write your code here
-module.exports = Student
+module.exports = Student(name,age,kclass){
+        Person.call(this,name,age);
+ 	this.kclass=kclass;	        
+ 	Student.prototype.introduce=function(){
+    	      var s1=new Person(name,age).introduce();
+    	      var s2="I am a Student. I am at Class "+this.kclass+".";
+    	      return s1+s2;
+        }	    	  
+}
