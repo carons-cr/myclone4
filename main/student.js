776777caa78a53616a17b79b3c46104c943712c4
carons-cr <2738794789@qq.com>
2017-06-12 20:00:51 +0800

    更改1

--- main/student.js
+++ main/student.js
@@ -6,6 +6,7 @@ module.exports = Student(name,age,kclass){
  	Student.prototype.introduce=function(){
     	      var s1=new Person(name,age).introduce();
     	      var s2=" I am a Student. I am at Class "+this.kclass+".";
-    	      return s1+s2;
+              var s=s1+s2;
+    	      return s;
         }	    	  
 }
